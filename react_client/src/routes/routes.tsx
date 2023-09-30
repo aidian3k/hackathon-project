b76a54e6b77e2b74c666c9affdd462617b38c5c8
KubaOrz <orz.kub@wp.pl>
2023-09-30 18:09:10 +0200

    starting page, register and login modals
    (cherry picked from commit 675d57013f15bcda27229634e2597a02b1643d0a)

--- react_client/src/routes/routes.tsx
+++ react_client/src/routes/routes.tsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "../pages/loginPage/loginPage";
+import { StartingPage } from "../pages/startingPage/StartingPage";
 
 export const router = createBrowserRouter([
   {
-    element: <LoginPage />,
+    element: <StartingPage />,
     errorElement: <div>wyjebało się</div>,
     path: "/",
   },
