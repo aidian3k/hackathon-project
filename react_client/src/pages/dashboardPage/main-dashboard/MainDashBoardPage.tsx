1ff49fcbaa1e9ca4218049e9b0318ccca730b784
cezarys8 <czarek.skorupski2001@gmail.com>
2023-10-01 10:31:55 +0200

    goal image depends on category

--- react_client/src/pages/dashboardPage/main-dashboard/MainDashBoardPage.tsx
+++ react_client/src/pages/dashboardPage/main-dashboard/MainDashBoardPage.tsx
@@ -22,14 +22,14 @@ export const MainDashBoardPage: FC = () => {
         </div>
         <div className={"grid grid-cols-2 gap-2"}>
           <GoalElement
-            typeOfGoal={"Mental"}
+            typeOfGoal={"Physical"}
             titleOfSubGoal={"Learning how to deal with stress"}
             description={
               "Mental health refers to a person's emotional, psychological, and social well-being. It encompasses an individual's ability to manage stress, cope with life's challenges, maintain positive relationships."
             }
           />
           <GoalElement
-            typeOfGoal={"Mental"}
+            typeOfGoal={"Physical"}
             titleOfSubGoal={"Learning how to deal with stress"}
             description={
               "Mental health refers to a person's emotional, psychological, and social well-being. It encompasses an individual's ability to manage stress, cope with life's challenges, maintain positive relationships."
@@ -64,7 +64,7 @@ export const MainDashBoardPage: FC = () => {
             }
           />
           <GoalElement
-            typeOfGoal={"Mental"}
+            typeOfGoal={"Physical"}
             titleOfSubGoal={"Learning how to deal with stress"}
             description={
               "Mental health refers to a person's emotional, psychological, and social well-being. It encompasses an individual's ability to manage stress, cope with life's challenges, maintain positive relationships."
@@ -91,9 +91,18 @@ export const GoalElement: FC<{
         <div className="md:shrink-0">
           <img
             className="h-48 w-full object-cover md:h-full md:w-48"
-            src="https://d2jx2rerrg6sh3.cloudfront.net/image-handler/ts/20220124072653/ri/1350/src/images/Article_Images/ImageForArticle_22028_16430272124432966.jpg"
-            alt="Modern building architecture"
+            src={props.typeOfGoal.toUpperCase() === "PHYSICAL"
+              ? "https://img.freepik.com/free-vector/healthy-lifestyle-concept-illustration_114360-5659.jpg?w=826&t=st=1696148264~exp=1696148864~hmac=3fa3a530f94eee9fab7f33685e04978cfe11efa96a66316887d1e4bf9aeed361"
+              : "https://d2jx2rerrg6sh3.cloudfront.net/image-handler/ts/20220124072653/ri/1350/src/images/Article_Images/ImageForArticle_22028_16430272124432966.jpg"}
+            alt=""
           />
+
+          {/* <img
+            className="h-48 w-full object-cover md:h-full md:w-48"
+            // src="https://d2jx2rerrg6sh3.cloudfront.net/image-handler/ts/20220124072653/ri/1350/src/images/Article_Images/ImageForArticle_22028_16430272124432966.jpg"
+            src="https://img.freepik.com/free-vector/healthy-lifestyle-concept-illustration_114360-5659.jpg?w=826&t=st=1696148264~exp=1696148864~hmac=3fa3a530f94eee9fab7f33685e04978cfe11efa96a66316887d1e4bf9aeed361"
+            alt=""
+          /> */}
         </div>
         <div className="p-8">
           <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
